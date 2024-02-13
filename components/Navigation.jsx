4b4a45fc4db5c4acff77e0b8e9595ac813780f60
Ghulam Ahmed <g_ahmeddd@yahoo.com>
2024-02-13 18:18:59 +0300

    x

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -4,12 +4,11 @@ import Logo from "./svg/Logo";
 function Navigation() {
   return (
     <nav className=" p-5 backdrop-blur-md fixed w-[642px] top-0 left-0 z-10 font-headline text-white">
-      <div className="flex items-center w-full">
+      <div className="flex items-center">
         <Logo />
-        <div className="w-full pl-10">
-          <ul className="flex w-full justify-around border-b-[1px] border-[#583922] pb-3 ">
+        <div className="pl-10">
+          <ul className="flex justify-around border-b-[1px] border-[#583922] pb-3 ">
             <li>Home</li>
-            <li>Design</li>
           </ul>
         </div>
       </div>
